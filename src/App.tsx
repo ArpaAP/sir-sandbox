6a0a6a9c4df9eea749f8f894f5781aca8b29fd85
ArpaAP <54466872+ArpaAP@users.noreply.github.com>
2022-01-11 08:24:19 +0000

    Mod/Fix: 각 결괏값 계산시 입력값의 비율을 사용하도록 변경, 버그 수정

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,12 @@ const App: React.FC = () => {
   const [S, setS] = React.useState(0);
   const [I, setI] = React.useState(0);
   const [R, setR] = React.useState(0);
-  const [beta, setBeta] = React.useState(0.5);
-  const [gamma, setGamma] = React.useState(0.5);
+  const [beta, setBeta] = React.useState(0.07);
+  const [gamma, setGamma] = React.useState(0.07);
+
+  const s = S / (S + I + R) || 0;
+  const i = I / (S + I + R) || 0;
+  const r = R / (S + I + R) || 0;
 
   return (
     <div
@@ -35,7 +39,7 @@ const App: React.FC = () => {
               <Card.Body className="py-3">
                 <Form>
                   <Form.Group as={Row} className="mb-3" controlId="Susceptible">
-                    <Col sm={9}>
+                    <Col sm={9} md={8}>
                       <Form.Label className="fw-bold" style={{ fontSize: 18 }}>
                         취약군 (
                         <b>
@@ -49,7 +53,7 @@ const App: React.FC = () => {
                         </Form.Text>
                       </div>
                     </Col>
-                    <Col sm={3} className="ms-auto">
+                    <Col sm={3} md={4} className="ms-auto">
                       <Form.Control
                         type="number"
                         value={S.toString()}
@@ -60,7 +64,7 @@ const App: React.FC = () => {
                     </Col>
                   </Form.Group>
                   <Form.Group as={Row} className="mb-3" controlId="Infected">
-                    <Col sm={9}>
+                    <Col sm={9} md={8}>
                       <Form.Label className="fw-bold" style={{ fontSize: 18 }}>
                         감염군 (
                         <b>
@@ -72,7 +76,7 @@ const App: React.FC = () => {
                         <Form.Text>병을 전염시킬 수 있는 개체의 수</Form.Text>
                       </div>
                     </Col>
-                    <Col sm={3} className="ms-auto">
+                    <Col sm={3} md={4} className="ms-auto">
                       <Form.Control
                         type="number"
                         value={I.toString()}
@@ -83,7 +87,7 @@ const App: React.FC = () => {
                     </Col>
                   </Form.Group>
                   <Form.Group as={Row} className="mb-3" controlId="Recovered">
-                    <Col sm={9}>
+                    <Col sm={9} md={8}>
                       <Form.Label className="fw-bold" style={{ fontSize: 18 }}>
                         회복군 (
                         <b>
@@ -98,7 +102,7 @@ const App: React.FC = () => {
                         </Form.Text>
                       </div>
                     </Col>
-                    <Col sm={3} className="ms-auto">
+                    <Col sm={3} md={4} className="ms-auto">
                       <Form.Control
                         type="number"
                         value={R.toString()}
@@ -137,7 +141,7 @@ const App: React.FC = () => {
                     </Col>
                   </Form.Group>
                   <Form.Group as={Row} className="mb-3" controlId="Beta">
-                    <Col sm={9}>
+                    <Col sm={8} md={9}>
                       <Form.Label className="fw-bold" style={{ fontSize: 18 }}>
                         회복률 (
                         <b>
@@ -152,7 +156,7 @@ const App: React.FC = () => {
                         </Form.Text>
                       </div>
                     </Col>
-                    <Col sm={3} className="ms-auto">
+                    <Col sm={4} md={3} className="ms-auto">
                       <Form.Control
                         type="number"
                         step="0.1"
@@ -165,7 +169,7 @@ const App: React.FC = () => {
                   </Form.Group>
 
                   <hr />
-                  <h6>초기 개체군 구성</h6>
+                  <h6>초기 개체군 구성 비율</h6>
 
                   <Form.Group>
                     <Col>
@@ -173,42 +177,43 @@ const App: React.FC = () => {
                         <ProgressBar>
                           <ProgressBar
                             variant="warning"
-                            now={(S / (S + I + R)) * 100}
+                            now={s * 100}
                             key={1}
                             label="취약군"
                             className="text-black"
                           />
                           <ProgressBar
                             variant="danger"
-                            now={(I / (S + I + R)) * 100}
+                            now={i * 100}
                             key={2}
                             label="감염군"
                           />
                           <ProgressBar
                             variant="success"
-                            now={(R / (S + I + R)) * 100}
+                            now={r * 100}
                             key={3}
                             label="회복군"
                           />
                         </ProgressBar>
                       ) : (
-                        <ProgressBar />
+                        <ProgressBar
+                          now={100}
+                          label="변수를 먼저 입력하세요"
+                          variant="dark"
+                        />
                       )}
 
                       <div className="pt-2">
                         <span style={{ color: "orange" }}>
-                          취약군:{" "}
-                          {Math.round((S / (S + I + R)) * 100 * 10) / 10 || 0}%
+                          취약군: {Math.round(s * 100 * 10) / 10 || 0}%
                         </span>{" "}
                         |{" "}
                         <span className="text-danger">
-                          감염군:{" "}
-                          {Math.round((I / (S + I + R)) * 100 * 10) / 10 || 0}%
+                          감염군: {Math.round(i * 100 * 10) / 10 || 0}%
                         </span>{" "}
                         |{" "}
                         <span className="text-success">
-                          회복군:{" "}
-                          {Math.round((R / (S + I + R)) * 100 * 10) / 10 || 0}%
+                          회복군: {Math.round(r * 100 * 10) / 10 || 0}%
                         </span>
                       </div>
                     </Col>
@@ -238,13 +243,13 @@ const App: React.FC = () => {
                       </Form.Label>
                       <div>
                         <Form.Text>
-                          <i>βI</i> 의 값과 같습니다.
+                          <i>βi</i> 의 값과 같습니다.
                         </Form.Text>
                       </div>
                     </Col>
                     <Col sm={3} className="ms-auto d-flex align-items-center">
                       <h5 className="fw-bold">
-                        {Math.round((beta * I * 1000) / 1000)}
+                        {Math.round(beta * i * 1000) / 1000}
                       </h5>
                     </Col>
                   </Form.Group>
@@ -253,19 +258,19 @@ const App: React.FC = () => {
                       <Form.Label className="fw-bold" style={{ fontSize: 18 }}>
                         시간에 따른 취약자 증가율 (
                         <b>
-                          <i>dS/dt </i>
+                          <i>ds/dt </i>
                         </b>
                         )
                       </Form.Label>
                       <div>
                         <Form.Text>
-                          <i>-βSI</i>, 즉 감염자로의 유출의 값과 같습니다.
+                          <i>-βsi</i>, 즉 감염자로의 유출의 값과 같습니다.
                         </Form.Text>
                       </div>
                     </Col>
                     <Col sm={3} className="ms-auto d-flex align-items-center">
                       <h5 className="fw-bold">
-                        {Math.round(-1 * beta * S * I * 1000) / 1000}
+                        {Math.round(-1 * beta * s * i * 1000) / 1000}
                       </h5>
                     </Col>
                   </Form.Group>
@@ -274,20 +279,20 @@ const App: React.FC = () => {
                       <Form.Label className="fw-bold" style={{ fontSize: 18 }}>
                         시간에 따른 감염자 증가율 (
                         <b>
-                          <i>dI/dt </i>
+                          <i>di/dt </i>
                         </b>
                         )
                       </Form.Label>
                       <div>
                         <Form.Text>
-                          <i>βSI-γI</i>, 즉 취약자에서의 유입에서 회복자로의
+                          <i>βsi-γi</i>, 즉 취약자에서의 유입에서 회복자로의
                           유출을 뺀 값과 같습니다.
                         </Form.Text>
                       </div>
                     </Col>
                     <Col sm={3} className="ms-auto d-flex align-items-center">
                       <h5 className="fw-bold">
-                        {Math.round((beta * S * I - gamma * I) * 1000) / 1000}
+                        {Math.round((beta * s * i - gamma * i) * 1000) / 1000}
                       </h5>
                     </Col>
                   </Form.Group>
@@ -296,19 +301,19 @@ const App: React.FC = () => {
                       <Form.Label className="fw-bold" style={{ fontSize: 18 }}>
                         시간에 따른 회복자 증가율 (
                         <b>
-                          <i>dR/dt </i>
+                          <i>dr/dt </i>
                         </b>
                         )
                       </Form.Label>
                       <div>
                         <Form.Text>
-                          <i>γI</i> 의 값과 같습니다.
+                          <i>γi</i> 의 값과 같습니다.
                         </Form.Text>
                       </div>
                     </Col>
                     <Col sm={3} className="ms-auto d-flex align-items-center">
                       <h5 className="fw-bold">
-                        {Math.round(gamma * I * 1000) / 1000}
+                        {Math.round(gamma * i * 1000) / 1000}
                       </h5>
                     </Col>
                   </Form.Group>
@@ -317,7 +322,7 @@ const App: React.FC = () => {
                       <Form.Label className="fw-bold" style={{ fontSize: 18 }}>
                         R<sub>0</sub> (
                         <b>
-                          <i>βS/γ </i>
+                          <i>βs/γ </i>
                         </b>
                         )
                       </Form.Label>
@@ -331,7 +336,10 @@ const App: React.FC = () => {
                     </Col>
                     <Col sm={3} className="ms-auto d-flex align-items-center">
                       <h5 className="fw-bold">
-                        {Math.round(((beta * S) / gamma) * 1000) / 1000}
+                        {gamma > 0
+                          ? Math.round(((beta * s) / gamma) * 1000) / 1000 ||
+                            "-"
+                          : "∞"}
                       </h5>
                     </Col>
                   </Form.Group>
